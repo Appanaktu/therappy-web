ab62dd89e83143d8478822bc32532bf2a37bbeb8
Dennis Bingenheimer <dbingenheimer@gmx.net>
2019-12-07 10:11:36 +0100

    neue HTMLs datenverwaltung.html, termine.html; Anpassungen in firebase.js

--- Webapp/static/javascript/firebase.js
+++ Webapp/static/javascript/firebase.js
@@ -10,6 +10,87 @@ var config = {
   "storageBucket": "iamphysiotherappy-17757.appspot.com"
 };
 
+// Initialisiere Datenank
+firebase.initializeApp(config);
+var db = firebase.firestore()
+
+// Schreib-Funktion
+function write_json_to_collection(collection_name, document_name, values) {
+
+  // Hole Collectionreferenz
+  var collectionRef = db.collection(collection_name)
+
+  // Schreibzugriff
+  if document_name == null{
+    collectionRef.add(values).then(function(docRef) {
+        console.log("Werte gespeichert unter der ID: ", docRef.id);
+    }).catch(function(error) {
+        console.error("Fehler beim schreiben der Daten: ", error);
+    });
+  } else {
+    collectionRef.doc(document_name).set(values).then(function() {
+        console.log("Werte wurden erfolgreich auf die Datenbank geschrieben");
+    }).catch(function(error) {
+        console.error("Fehler beim schreiben der Daten: ", error);
+    });
+  };
+};
+
+// Lese-Funktionen
+function get_all_docs_from_collection(collection_name){
+
+  // Hole Collectionreferenz
+  var collectionRef = db.collection(collection_name)
+
+  // Lesezugriff
+  collectionRef.get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+    });
+});
+};
+
+function get_json_from_collection_by_query(collection_name, trait, operator, value) {
+
+  // Hole Collectionreferenz
+  var collectionRef = db.collection(collection_name)
+
+  // Lesezugriff
+  collectionRef.where(trait, operator, value)
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+};
+
+function get_json_from_collection_by_doc(collection_name, document_name) {;
+
+  // Hole Collectionreferenz
+  var collectionRef = db.collection(collection_name)
+
+  // Hole Dokumentenreferzenz
+  var docRef = collectionRef.doc(document_name);
+
+  // Lesezugriff
+  docRef.get().then(function(doc) {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+  }).catch(function(error) {
+    console.log("Error getting document:", error);
+  });
+};
+
 //Wenn Website geladen
 document.addEventListener("DOMContentLoaded", function() {
 	firebase.initializeApp(config);
@@ -50,13 +131,13 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
-  //ALle Einträge ausgeben erstmal in Konsole
-  allButton.addEventListener("click", function() {
-    db.collection("Nutzer").get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-        });
-    });
+  // ALle Einträge ausgeben erstmal in Konsole
+  // allButton.addEventListener("click", function() {
+  //   db.collection("Nutzer").get().then(function(querySnapshot) {
+  //       querySnapshot.forEach(function(doc) {
+  //           // doc.data() is never undefined for query doc snapshots
+  //           console.log(doc.id, " => ", doc.data());
+  //       });
+  //   });
   });
 });
